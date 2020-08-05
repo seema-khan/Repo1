263eeb877a556895acda1955021503372184f722
Seema <theseemakhan@gmail.com>
2020-08-04 22:09:28 -0400

    Updated print statement

--- Samples/src/demo/Test1.java
+++ Samples/src/demo/Test1.java
@@ -3,7 +3,7 @@ package demo;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Seema");
+		System.out.println("*****************Hello Seema*****************");
 	}
 
 }
